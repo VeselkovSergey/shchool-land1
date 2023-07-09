7109965a9effb3cff449f7e9f16bfc55d5f3e951
Sergey <veselok@gmail.com>
2023-07-09 14:40:57 +0300

    github webhook

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'github'], function () {
+
+    Route::post('/push', function () {
+        // sudo -u www-data ssh-keygen - генерим ssh ключи под www-data
+        // chmod 600 /var/www/.ssh/id_rsa - для работы git pull под www-data
+        echo 'git pull start' . PHP_EOL;
+        echo shell_exec('git pull');
+        echo 'git pull complete' . PHP_EOL;
+
+        echo 'php artisan migrate start' . PHP_EOL;
+        \Illuminate\Support\Facades\Artisan::call('migrate');
+        echo 'php artisan migrate complete' . PHP_EOL;
+    })->name('github-push');
+
+});
